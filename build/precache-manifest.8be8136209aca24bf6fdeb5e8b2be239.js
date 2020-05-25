6319731feed7e60251f5b013204cbebe4f9bc02e
Clive Machado <clive.machado@raweng.com>
2020-05-25 08:34:17 +0530

    remove build from gitignore

---
+++ build/precache-manifest.8be8136209aca24bf6fdeb5e8b2be239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044a3afa316fbffbcd7921ad8a0a27b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3ca17473056769f0a2d",
+    "url": "/static/css/main.ef91a697.chunk.css"
+  },
+  {
+    "revision": "855690c0cfd18244bfc2",
+    "url": "/static/js/2.35b42d15.chunk.js"
+  },
+  {
+    "revision": "e3ca17473056769f0a2d",
+    "url": "/static/js/main.ce286f92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
