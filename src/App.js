d4fb1fc48b174f83ec226cca86f59902895535e8
Clive Machado <clive.machado@raweng.com>
2020-05-25 08:24:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {
+  Scene,
+  Engine,
+  UniversalCamera,
+  Vector3,
+  HemisphericLight,
+  Mesh,
+  Vector2,
+} from "babylonjs";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // this.init();
+  }
+
+  init = () => {
+    let canvas = document.getElementById("renderCanvas");
+    let engine = new Engine(canvas, true, {
+      preserveDrawingBuffer: true,
+      stencil: true,
+    });
+    let createScene = () => {
+      let scene = new Scene(engine);
+      let camera = new UniversalCamera(
+        "camera1",
+        new Vector3(0, 5, -10),
+        scene
+      );
+      camera.setTarget(Vector3.Zero());
+      camera.attachControl(canvas, false);
+      let light = new HemisphericLight("light1", new Vector3(0, 1, 3), scene);
+      let sphere = new Mesh.CreateSphere(
+        "sphere1",
+        16,
+        2,
+        scene,
+        false,
+        Mesh.FRONTSIDE
+      );
+      sphere.position.y = 1;
+      let ground = new Mesh.CreateGround(
+        "ground1",
+        200,
+        100,
+        100,
+        scene,
+        false
+      );
+      return scene;
+    };
+    let scene = createScene();
+    engine.runRenderLoop(function () {
+      scene.render();
+    });
+  };
+
+  componentDidMount() {
+    this.init();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <canvas id="renderCanvas" className="theFirstCanvas"></canvas>
+      </div>
+    );
+  }
 }
 
 export default App;
