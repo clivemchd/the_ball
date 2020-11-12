8582183ba668d49d5db06d9e1e8ea52fde9896e6
Clive Machado <clive.machado@raweng.com>
2020-11-12 10:31:04 +0530

    adds arc rotate camera

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from "react";
 import {
   Scene,
   Engine,
-  UniversalCamera,
+	UniversalCamera,
+	ArcRotateCamera,
+	FollowCamera,
   Vector3,
   HemisphericLight,
   Mesh,
@@ -14,7 +16,6 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    // this.init();
   }
 
   init = () => {
@@ -25,13 +26,15 @@ class App extends Component {
     });
     let createScene = () => {
       let scene = new Scene(engine);
-      let camera = new UniversalCamera(
-        "camera1",
-        new Vector3(0, 5, -10),
-        scene
-      );
+      // let camera = new UniversalCamera(
+      //   "camera1",
+      //   new Vector3(0, 5, -10),
+      //   scene
+      // );
       // camera.setTarget(Vector3.Zero());
-      camera.attachControl(canvas, false);
+			let camera = new ArcRotateCamera("camera1", 0, 0, 10, new Vector3(5, 1, 20), scene)
+			camera.setPosition(new Vector3(5, 1, 20))
+			camera.attachControl(canvas, false);
       let light = new HemisphericLight("light1", new Vector3(0, 1, 3), scene);
       let sphere = new Mesh.CreateSphere(
         "sphere1",
@@ -40,7 +43,9 @@ class App extends Component {
         scene,
         false,
         Mesh.FRONTSIDE
-      );
+			);
+			let cube = new Mesh.CreateBox("cube1", 2, scene, false, Mesh.FRONTSIDE)
+			cube.position.y = 5
       sphere.position.y = 1;
       let ground = new Mesh.CreateGround(
         "ground1",
@@ -52,12 +57,12 @@ class App extends Component {
       );
       scene.collisionsEnabled = true;
       ground.checkCollisions = true;
-      sphere.checkCollisions = true;
-      camera.checkCollisions = true;
+      // sphere.checkCollisions = true;
+      // camera.checkCollisions = true;
 
-      camera.onCollide = (mesh) => {
-        console.log("Colided", mesh);
-      };
+      // camera.onCollide = (mesh) => {
+      //   console.log("Colided", mesh);
+      // };
       return scene;
     };
     let scene = createScene();
